96e16785aff6338a865becf0c5db986624f625f9
Hanut Singh Gusain <hanutsingh@gmail.com>
2018-06-16 15:47:46 +0530

    Added more tests. Finished init function for datastore and test cases for the same

--- tests/datastore.spec.js
+++ tests/datastore.spec.js
@@ -1,6 +1,9 @@
 const expect = require('chai').expect
+const fs = require('fs')
 const path = require('path')
 const datastore = require('../src/datastore')
+const VALID_PATH = process.cwd() + path.sep + 'xchange.db'
+const INVALID_PATH = 'X:' + path.sep + 'xchange.db'
 
 describe('Datastore Module', function() {
 
@@ -14,19 +17,40 @@ describe('Datastore Module', function() {
 
   describe('#init()', function() {
     it('should return a promise', function(done) {
-      let promise = datastore.init(process.cwd() + path.sep + 'xchange.db')
+      let promise = datastore.init(VALID_PATH).then(() => {
+        fs.unlink(VALID_PATH, () => {
+          //do nothing
+        })
+      }).catch(error => {
+        //do nothing
+      })
       expect(promise).to.be.a('promise')
       done()
     })
 
     it('should throw an error if db creation failed', function(done) {
-      datastore.init('X:' + path.sep + 'xchange.db').then(db => {
+      datastore.init(INVALID_PATH).then(db => {
         done(new Error('Bad resolve on init(). Should have thrown error'))
       }).catch(error => {
         expect(error).to.be.an('error')
         done()
       })
     })
+
+    it('should create xchange.db at the given path', function(done) {
+      let promise = datastore.init(VALID_PATH).then(() => {
+        fs.stat(VALID_PATH, function(err, stats){
+          if(err) {
+            done(err)
+          } else {
+            done()
+          }
+        })
+      }).catch(error => {
+        done(error)
+      })
+    })
+
   })
 
 
